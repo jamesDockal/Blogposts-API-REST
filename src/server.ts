417a09505f46f9303c368834acb2723f23a15f41
James Dockal <48695399+jamesDockal@users.noreply.github.com>
2021-07-10 01:48:45 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ dotenv.config();
 
 // start the server in localhost
 app.listen(process.env.SERVER_PORT || 3000, () =>
-  console.log(`Server Running on port ${process.env.SERVER_PORT || 3000}`)
+  console.log(`Server Running on port ${process.env.PORT || 3000}`)
 );
