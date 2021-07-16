ddb8a5040b1038e55203e6bc0b9166ef70fe50fd
james dockal <jamesistranho@gmail.com>
2021-07-16 17:41:25 -0300

    reset src folder

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 // start the database
 require("./database");
+// library to doc the api
+var swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+var swagger_json_1 = __importDefault(require("./swagger.json"));
 // routes file
 var userRoutes_1 = __importDefault(require("./Routes/userRoutes"));
 var blogpostRoutes_1 = __importDefault(require("./Routes/blogpostRoutes"));
@@ -14,6 +17,8 @@ var app = express_1.default();
 app.use(cors_1.default());
 // code to the express can ready json on the body
 app.use(express_1.default.json());
+// documentation of the api
+app.use("/", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 // user's routes
 app.use("/user", userRoutes_1.default);
 // blogpost' routes
