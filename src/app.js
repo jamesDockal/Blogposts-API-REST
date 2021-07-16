6abddbe383d7d6a7662d5b52837bf5fe782656d8
james dockal <jamesistranho@gmail.com>
2021-07-16 18:16:29 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ var cors_1 = __importDefault(require("cors"));
 // start the database
 require("./database");
 // library to doc the api
-var swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-var swagger_json_1 = __importDefault(require("./swagger.json"));
+// import swaggerUI from "swagger-ui-express";
+// import swaggerDocs from "./swagger.json";
 // routes file
 var userRoutes_1 = __importDefault(require("./Routes/userRoutes"));
 var blogpostRoutes_1 = __importDefault(require("./Routes/blogpostRoutes"));
@@ -18,7 +18,7 @@ app.use(cors_1.default());
 // code to the express can ready json on the body
 app.use(express_1.default.json());
 // documentation of the api
-app.use("/", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
+// app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 // user's routes
 app.use("/user", userRoutes_1.default);
 // blogpost' routes
